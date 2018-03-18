9ce7177f68078c4e90e4d554f30dcf5243838735
wheelsmanx <wheelsmanx@gmail.com>
2018-03-18 16:35:12 -0700

    added sleep function
    added a timer function

--- emailBot.py
+++ emailBot.py
@@ -2,14 +2,18 @@
 # to multiple users 
 # from your Gmail account 
 import smtplib
+import time 
  
 # list of email_id to send the mail
 li = ["justinshagerty@gmail.com"]
- 
-for i in range(len(li)):
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login("wheelsmanx", "candice..")
-    message = "Message_you_need_to_send"
-    s.sendmail("wheelsmanx@gmail.com", li[i], message)
-    s.quit
+b = 0
+while(b < 5):
+    for i in range(len(li)):
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.starttls()
+        s.login("wheelsmanx", "candice..")
+        message = "Message_you_need_to_send"
+        s.sendmail("wheelsmanx@gmail.com", li[i], message)
+        s.quit
+        b = b + 1
+        time.sleep(60)
