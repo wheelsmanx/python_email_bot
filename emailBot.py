6ccfea017c9f58aa37473ada92989fe97d70eb86
wheelsmanx <wheelsmanx@gmail.com>
2018-03-18 21:28:19 -0700

    added a discription
    I wrote some more definitions.

--- emailBot.py
+++ emailBot.py
@@ -1,6 +1,39 @@
+#By Justin Hagerty
+#Date: 3/18/2018
 # Python code to illustrate Sending mail 
-# to multiple users 
-# from your Gmail account 
+# Notes include how to make this script a service in ubuntu
+
+"""
+what actually makes this repo is
+the fact that I used upstart in ubuntu to make this
+a service.
+
+note if you are running ubuntu 16+
+you must run first.
+
+sudo apt-get install upstart-sysv
+
+the reason being is that they did away with upstart in 16+ and replaced it
+with systemd
+
+1.) Make the file emailBot.conf in /etc/init
+
+<beginFile>
+description = "your description here"
+
+start on runlevel [2345]
+stop on runlevel [016]
+
+respawn
+
+//put this file here at this location
+exec python /pythonScript/emailBot.py
+</beginFile>
+
+2.)run the command initctl reload-configuration
+
+"""
+
 import smtplib
 import time 
  
