f72b7190c758a7edf57aa4170e1066e80086e37f
wheelsmanx <wheelsmanx@gmail.com>
2018-03-18 17:37:14 -0700

    fixed some things
    fixed some other stuff

--- emailBot.py
+++ emailBot.py
@@ -6,14 +6,12 @@ import time
  
 # list of email_id to send the mail
 li = ["justinshagerty@gmail.com"]
-b = 0
-while(b < 5):
-    for i in range(len(li)):
-        s = smtplib.SMTP('smtp.gmail.com', 587)
-        s.starttls()
-        s.login("wheelsmanx", "candice..")
-        message = "Message_you_need_to_send"
-        s.sendmail("wheelsmanx@gmail.com", li[i], message)
-        s.quit
-        b = b + 1
-        time.sleep(60)
+
+for i in range(len(li)):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("", "")
+    message = "Message_you_need_to_send"
+    s.sendmail("wheelsmanx@gmail.com", li[i], message)
+    s.quit
+
